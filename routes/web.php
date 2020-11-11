eef9f8974a7251c20df940ebcce9471e8ede2795
李譯騏 <patrick14754@gmail.com>
2020-11-11 10:37:07 +0800

    練習2-1, 2-2,會遇到 Mass Assignment 的警告

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    \app\Post::create([
+        'title'=>'test title',
+        'content'=>'test content'
+    ]);
 });
 
 Route::get('posts', ['as' => 'posts.index',   'uses' => 'PostsController@index']);
