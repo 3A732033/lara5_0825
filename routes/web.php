ebf2b5c61a48af62498243e33afee306bfd8fb6c
李譯騏 <patrick14754@gmail.com>
2020-11-11 11:24:53 +0800

    練習4-1

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,14 @@ Route::get('/', function () {
 //    $post=\App\Post::find(1);
 //    dd($post);
 
-    $posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
-    dd($posts);
+//    $posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
+//    dd($posts);
+
+    $post=\App\Post::find(1);
+    $post->update([
+        'title'=>'updated title',
+        'content'=>'updated content',
+    ]);
 
 
 });
