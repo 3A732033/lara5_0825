14a50461ac8eaff9bbef262515da4140faf9e532
李譯騏 <patrick14754@gmail.com>
2020-11-18 00:42:14 +0800

    練習7-2在comments.php增加關聯

--- app/comments.php
+++ app/comments.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Post;
 class comments extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'content'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
