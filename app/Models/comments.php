4c2e59c82977e2c03bdca9478479d46c2a2ac90c
李譯騏 <patrick14754@gmail.com>
2020-12-07 00:30:51 +0800

    建立Comment.php和設定 app/Models/Comment.php 的 fillable 屬性

--- app/Models/comments.php
+++ app/Models/comments.php
@@ -5,14 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Post;
-class comments extends Model
+
+class Comment extends Model
 {
-    protected $fillable = [
+    use HasFactory;
+    protected $fillable =[
         'content'
     ];
 
-    public function post()
-    {
-        return $this->belongsTo(Post::class);
-    }
 }
